99dbc63db9d0eea91e53672de0d634a2008c2691
Osman Baskaya <obaskaya@ku.edu.tr>
2013-05-14 10:13:08 +0300

    dataset and src created

---
+++ src/data_fetch.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+__date__ = "14.06.2013"
+
+""" This module fetches the data from http://ideas.repec.org/ """
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
